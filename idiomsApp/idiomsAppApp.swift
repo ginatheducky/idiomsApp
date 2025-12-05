659c085432b808553a511f364c1360a6167fa021
ginatheducky <ginatheducky@gmail.com>
2025-12-05 18:31:36 +0100

    Initial Commit

---
+++ idiomsApp/idiomsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  idiomsAppApp.swift
+//  idiomsApp
+//
+//  Created by Gina on 05.12.25.
+//
+
+import SwiftUI
+
+@main
+struct idiomsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
